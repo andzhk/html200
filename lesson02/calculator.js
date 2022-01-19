ff8dec3c8e390d032e8c7e9e1e077db71669294b
andzhk <87098864+andzhk@users.noreply.github.com>
2022-01-19 14:15:22 -0800

    Add files via upload

---
+++ lesson02/calculator.js
@@ -0,0 +1,22 @@
+
+let x = 35;
+let y = 50;
+let z = x + y;
+console.log(z);
+
+console.log (4*4);
+
+let x = 9;
+
+if (x*5>=45) {
+  console.log(x);
+}
+
+let x = 50;
+Number(x) + 49;
+console.log(x+49);
+
+let x = 200;
+let y = 400;
+let z = x*y;
+console.log(z);
